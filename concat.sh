9413f2ca77d2b21adcae3a91df51b89658388c21
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2017-12-19 13:36:17 +0900

    first commit

---
+++ concat.sh
@@ -0,0 +1,5 @@
+cat head.html demo-1-body.html tail.html > demo-1.html
+cat head.html demo-2-body.html tail.html > demo-2.html
+cat head.html demo-3-body.html tail.html > demo-3.html
+cat head.html demo-4-body.html tail.html > demo-4.html
+cat head.html demo-5-body.html tail.html > demo-5.html
